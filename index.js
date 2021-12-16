090cdec2d01175c6d6473d632988d027d3b492e5
Lily <65602967+LilyyDev@users.noreply.github.com>
2021-12-16 18:09:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ let config = {
 function gS() {
     $.getJSON(config.lanyard, (data) => {
         data = data.data;
-        if (data.listening_to_spotify == true) {
-            $("#spotify").html(`&bull; Listening to <b>${data.spotify.song}</b>`)
-        } else {
-            $("#spotify").addClass("invis")
-        }
-        if (data.discord_status == "dnd" || data.discord_status == "online") {
-            $("#status").html("Online")
+        spotifyLink = "https://open.spotify.com/track/" + data.spotify.track_id;
+        if (data.active_on_discord_mobile) {
+            $("#status").html("Online On Mobile")
             $("#status_icon").css("color", "#abffbf")
-        } else if (data.discord_status == "idle") {
-            $("#status").html("Away")
-            $("#status_icon").css("color", "#F2A6AB")
-        } else if(data.discord_status == "offline") {
-            $("#status").html("Offline")
-            $("#status_icon").css("color", "#d4d4d4")
+        }
+        else {
+            if (data.listening_to_spotify == true) {
+                $("#spotify").html(`&bull; Listening to:<b><u><a href="${spotifyLink}" target="_blank">${data.spotify.artist} - ${data.spotify.song}</a></u></b>`)
+                $("#status_icon").css("color", "#abffbf")
+            } else {
+                $("#spotify").addClass("invis")
+            }
+            if (data.discord_status == "dnd" || data.discord_status == "online") {
+                $("#status").html("Online")
+                $("#status_icon").css("color", "#abffbf")
+            } else if (data.discord_status == "idle") {
+                $("#status").html("Away")
+                $("#status_icon").css("color", "#F2A6AB")
+            } else if(data.discord_status == "offline") {
+                $("#status").html("Offline")
+                $("#status_icon").css("color", "#d4d4d4")
+            }
         }
     })
 }
